f132310cfef3d71a36b3f7d6d46cd70a59723f14
Philip Cortes <philip.cortes@gmail.com>
2013-04-13 17:05:47 -0700

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrapey::Application.config.secret_token = 'deb375fd5ac74ff90518b3139d29b7ee3e1cb910303e7519f9d2607f565064c6036b850bcce858b2a2b14a2542eb23ac4706c43db00d68fd2f64950e498f1f13'
